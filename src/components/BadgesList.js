26dd898128bfe9743bfbf3fe366e53a143f9588c
Carolina Salazar <cindy.s.r.28@hotmail.com>
2020-10-06 12:20:30 -0500

    add border for badgeslist

--- src/components/BadgesList.js
+++ src/components/BadgesList.js
@@ -1,19 +1,21 @@
 import React from "react";
-
+import "../components/styles/BadgesList.css";
 class BadgesList extends React.Component {
   render() {
     return (
       <ul className="list-unstyled">
         {this.props.badges.map((badge) => {
           return (
-            <li key={badge.id}>
-              <img src="{badge.avatar}" alt="Imagen de Badge" />
-              <p>
-                {badge.firstName} {badge.lastName}
-              </p>
-              <span className="Twitter__logo">@{badge.twitter}</span>
-              <p>{badge.jobTitle}</p>
-            </li>
+            <div className="Badges___list">
+              <li key={badge.id}>
+                <img src="{badge.avatarUrl}" alt="Imagen de Badge" />
+                <p>
+                  {badge.firstName} {badge.lastName}
+                </p>
+                <span className="Twitter__logo">@{badge.twitter}</span>
+                <p>{badge.jobTitle}</p>
+              </li>
+            </div>
           );
         })}
       </ul>
